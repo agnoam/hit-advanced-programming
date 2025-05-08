2af6f66455ee0b9e4e27bbacecf58b4915916036
agnoam <agnoam.aghai.ooo.123@gmail.com>
2025-05-08 12:12:10 +0300

    Added docs and doxygen for `class_work_8`

--- class_works/class_work_8.c
+++ class_works/class_work_8.c
@@ -3,44 +3,63 @@
 #include <math.h>
 
 /*
-    Elaboration:
-    A triangle bottom matrix is a term from the linear algebra.
+    Elaboration - triangular matrix:
+        A triangular matrix is a term from the linear algebra.
 
-    Definition of a triangle matrix:
-    The matrix should be in NxN
-
-    Example for bottom triangle matrix:
-        0 1 2 3 4
-        ---------
-    0 | 4 0 0 0 0 | 
-    1 | 6 8 0 0 0 |
-    2 | 9 1 3 0 0 |
-    3 | 2 5 7 4 0 |
-    4 | 1 7 3 8 1 |
-        ---------
-    
-    Example for top triangle matrix:
-        0 1 2 3 4
-        ---------
-    0 | 4 7 1 2 3 |
-    1 | 0 6 5 3 9 |
-    2 | 0 0 1 6 8 |
-    3 | 0 0 0 2 1 |
-    4 | 0 0 0 0 5 |
-        ---------
+        Definition of a triangular matrix:
+        The matrix should be in NxN, which all the non-0 values will be in the range `j =< i` or `j >= i`.
+        Where `j` stands for the columns and `i` stands for the rows indicators.
+        Which will form an triangle like shape in the matrix.
+        
+        Example for lower triangular matrix:
+            0 1 2 3 4
+            ---------
+        0 | 4 0 0 0 0 | 
+        1 | 6 8 0 0 0 |
+        2 | 9 1 3 0 0 |
+        3 | 2 5 7 4 0 |
+        4 | 1 7 3 8 1 |
+            ---------
+        
+        Example for upper triangular matrix:
+            0 1 2 3 4
+            ---------
+        0 | 4 7 1 2 3 |
+        1 | 0 6 5 3 9 |
+        2 | 0 0 1 6 8 |
+        3 | 0 0 0 2 1 |
+        4 | 0 0 0 0 5 |
+            ---------
 */
 
 /*
-* Definition of a callback function of the "get_element" functions, which helps in writing less code
+    Elaboration - callback function:
+        A Callback function is actually a pointer/reference to a function that can be passed 
+        into another function to be able to make a "generic function.
+        In `c` we will have to make a typedef (more info can be found in the previous class_works),
+        which will make an alias for a pointer that stands for a specific function with a specific arguments.
+        
+        For example:
+        typedef <function's_return_type> (*<name_of_the_typedef>)(<...argument_list_of_the_function>);
+
+        Usage example can be found at `print_trianglar_matrix` calls in the `main` function.
 */
-typedef int (*GetElementCallback)(int** triangle_memory, int row, int col);
 
 /**
- * Generate bottom triangle matrix and returning memory efficient matrix representation
+ * Definition of a callback function of the `get_element_*` functions.
  */
-int** generate_bottom_triangle_matrix(int rows) {
-    int** triangle_memory = (int**) malloc(sizeof(int*) * rows);
-    if (!triangle_memory)
+typedef int (*GetElementCallback)(int** triangular_memory, int row, int col);
+
+/**
+ * Generates a dynamically allocated lower triangular matrix with random integers.
+ *
+ * @param rows The number of rows for the triangular matrix.
+ * @return A pointer to an array of int pointers representing the lower triangular matrix,
+ * or `NULL` if memory allocation fails.
+ */
+int** generate_lower_triangular_matrix(int rows) {
+    int** triangular_memory = (int**) malloc(sizeof(int*) * rows);
+    if (!triangular_memory)
         return NULL;
 
     // Saving into the storage
@@ -49,27 +68,42 @@ int** generate_bottom_triangle_matrix(int rows) {
         if (!cols_arr)
             return NULL;
 
-        triangle_memory[i] = cols_arr;
+        triangular_memory[i] = cols_arr;
         for (int j = 0; j <= i; j++)
             cols_arr[j] = rand() % (1 - 9 + 1) + 1;
     }
 
-    return triangle_memory;
+    return triangular_memory;
 }
 
-int get_element_bottom_triangle(int** triangle_memory, int row, int col) {
+/**
+ * Retrieves an element from a lower triangular memory structure, returning 0 for out-of-bound upper elements.
+ *
+ * @param triangular_memory The 2D lower triangular memory structure.
+ * @param row The row index of the desired element.
+ * @param col The column index of the desired element.
+ * 
+ * @return The value at the specified position, or 0 if the column index exceeds the row index.
+ */
+int get_element_lower_triangular_memory(int** triangular_memory, int row, int col) {
     if (col > row)
         return 0;
 
-    return triangle_memory[row][col];
+    return triangular_memory[row][col];
 }
 
-int** generate_top_triangle_matrix(int dimension_n) {
-    int** triangle_memory = (int**) malloc(sizeof(int*) * dimension_n);
-    if (!triangle_memory)
+/**
+ * Generates a dynamically allocated upper triangular matrix with random integers.
+ *
+ * @param dimension_n The number of rows and columns for the square matrix.
+ * @return A pointer to an array of int pointers representing the upper triangular matrix,
+ * or `NULL` if memory allocation fails.
+ */
+int** generate_upper_triangular_matrix(int dimension_n) {
+    int** triangular_memory = (int**) malloc(sizeof(int*) * dimension_n);
+    if (!triangular_memory)
         return NULL;
 
-    int dummy = 1;
     // Saving into the storage
     for (int i = 0; i < dimension_n; i++) {
         // Dimension N = number of cols and rows
@@ -77,60 +111,83 @@ int** generate_top_triangle_matrix(int dimension_n) {
         if (!cols_arr)
             return NULL;
 
-        triangle_memory[i] = cols_arr;
+        triangular_memory[i] = cols_arr;
         for (int j = 0; j < dimension_n - i; j++)
-            cols_arr[j] = dummy++;
-            // cols_arr[j] = rand() % (1 - 9 + 1) + 1;
+            cols_arr[j] = rand() % (1 - 9 + 1) + 1;
     }
 
-    return triangle_memory;
+    return triangular_memory;
 }
 
-int get_element_top_triangle(int** triangle_memory, int row, int col) {
+/**
+ * Retrieves an element from an upper triangular memory structure, returning 0 for out-of-bound lower elements.
+ *
+ * @param triangular_memory The 2D upper triangular memory structure.
+ * @param row The row index of the desired element.
+ * @param col The column index of the desired element.
+ * 
+ * @return The value at the specified position, or 0 if the column index is less than the row index.
+ */
+int get_element_upper_trianglar_memory(int** triangular_memory, int row, int col) {
     if (col < row)
         return 0;
 
-    return triangle_memory[row][col-row];
+    /* Because of the  */
+    return triangular_memory[row][col-row];
 }
 
-void print_triangle_matrix(int** triangle_memory, int dimension_n, GetElementCallback get_element) {
+/**
+ * Prints a triangular matrix using a callback to retrieve its elements. 
+ *
+ * @param triangular_memory The 2D triangular memory structure to print.
+ * @param dimension_n The number of rows and columns in the square matrix view.
+ * @param get_element A callback function to retrieve elements from the matrix.
+ */
+void print_trianglar_matrix(int** triangular_memory, int dimension_n, GetElementCallback get_element) {
     printf("[\n");
     for (int i = 0; i < dimension_n; i++) {
         for (int j = 0; j < dimension_n; j++)
-            printf("%3d ", get_element(triangle_memory, i, j));
+            printf("%d ", get_element(triangular_memory, i, j));
         printf("\n");
     }
     printf("]\n");
 }
 
 /**
- * This function assume the
+ * Frees the memory allocated for a triangular memory.
+ *
+ * @param triangular_memory The 2D triangular memory to free.
+ * @param rows The number of rows in the matrix.
  */
-int free_triangle(int** triangle_memory, int rows) {
+void free_triangle(int** triangular_memory, int rows) {
     for (int i = 0; i < rows; i++)
-        free(triangle_memory[i]);
-    free(triangle_memory);
+        free(triangular_memory[i]);
+    free(triangular_memory);
 }
 
 int main() {
     int dimension_n = 5;
-    int** bottom_triangle_memory = generate_bottom_triangle_matrix(dimension_n);
-    if (!bottom_triangle_memory) {
+
+    printf("Generating matrices from %d order (it means %dx%d)\n", dimension_n, dimension_n, dimension_n);
+    int** bottom_triangular_memory = generate_lower_triangular_matrix(dimension_n);
+    if (!bottom_triangular_memory) {
         perror("Memory allocation failed");
         return EXIT_FAILURE;
     }
     
-    print_triangle_matrix(bottom_triangle_memory, dimension_n, get_element_bottom_triangle);
+    printf("The lower triangular matrix is: ");
+    print_trianglar_matrix(bottom_triangular_memory, dimension_n, get_element_lower_triangular_memory);
 
-    int** top_triangle_memory = generate_top_triangle_matrix(dimension_n);
-    if (!top_triangle_memory) {
+    int** top_triangular_memory = generate_upper_triangular_matrix(dimension_n);
+    if (!top_triangular_memory) {
         perror("Memory allocation failed");
         return EXIT_FAILURE;
     }
 
-    print_triangle_matrix(top_triangle_memory, dimension_n, get_element_top_triangle);
+    printf("The upper triangular matrix is: ");
+    print_trianglar_matrix(top_triangular_memory, dimension_n, get_element_upper_trianglar_memory);
     
-    free_triangle(bottom_triangle_memory, dimension_n);
-    free_triangle(top_triangle_memory, dimension_n);
+    free_triangle(bottom_triangular_memory, dimension_n);
+    free_triangle(top_triangular_memory, dimension_n);
     return 0;
 }
