179c3b3410f9f1f49f6de251af8741fbcd36ea44
agnoam <agnoam.aghai.ooo.123@gmail.com>
2025-05-15 21:32:00 +0300

    Added doxygen and fixed `update_file` for `txt` file

--- class_works/class_work_12.c
+++ class_works/class_work_12.c
@@ -2,31 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ID_LENGTH 9
+#define NAME_LENGTH 30
+#define TEXT_LINE_LENGTH (ID_LENGTH + NAME_LENGTH + 10 + 3)
+
 // Anonymous struct
 typedef struct {
-    // Adding 1 extra for indcluding the \0 at the end
-    char id[10]; // Originally 9
-    char name[31]; // Originally 30
+    char id[ID_LENGTH + 1];
+    char name[NAME_LENGTH + 1];
     double cart;
 } CartRecord;
 
+/**
+ * This function creates a new cart record with the given `ID`, `name`, and `cart` value.
+ * 
+ * @param id The ID of the new cart record.
+ * @param name The name of the new cart record.
+ * @param cart The cart value of the new cart record.
+ * 
+ * @return The new cart record. If the `ID` or `name` is not provided, an empty cart record is returned.
+ */
 CartRecord new_cart(char id[], char name[], double cart) {
     int i = 0;
     CartRecord new_cart;
 
-    // Hard copying the strings into the new struct
-    for (i = 0; i < 9; i++)
-        new_cart.id[i] = id[i];
-    new_cart.id[i] = '\0'; // Putting trailing \0
-
-    for (i = 0; i < 30; i++)
-        new_cart.name[i] = name[i];
-    new_cart.name[i] = '\0'; // Putting trailing \0
-
+    if (!id || !name) {
+        perror("ID or name is not provided");
+        CartRecord empty = { "", "", 0 };
+        return empty;
+    }
+    
     new_cart.cart = cart;
+    strncpy(new_cart.id, id, ID_LENGTH);
+    strncpy(new_cart.name, name, NAME_LENGTH);
+    
+    // Adding trailing '\0' in the last character in both of the strings
+    new_cart.id[ID_LENGTH] = '\0'; // Putting trailing \0
+    new_cart.name[NAME_LENGTH] = '\0'; // Putting trailing \0
+
     return new_cart;
 }
 
+/**
+ * This function creates a file and writes a record into it. The record can be written in binary or text format.
+ * 
+ * @param record The record to write into the file.
+ * @param filename The name of the file to create.
+ * @param is_binary A flag indicating whether the file is binary or not.
+ */
 void create_file(CartRecord record, char* filename, int is_binary) {
     FILE* file = fopen(filename, is_binary ? "ab" : "a");
     if (!file) {
@@ -38,7 +61,18 @@ void create_file(CartRecord record, char* filename, int is_binary) {
     fclose(file);
 }
 
-int update_file(char id[], int cart_sum, char* filename) {
+/**
+ * This function updates a record in a file. If the file is binary, it seeks the record and overwrites it.
+ * If the file is text, it creates a temporary file, writes the updated records into it, and then copies it back to the original file.
+ * 
+ * @param id The ID of the record to update.
+ * @param cart_sum The new cart sum to update.
+ * @param filename The name of the file to update.
+ * @param is_binary A flag indicating whether the file is binary or not.
+ *  
+ * @return 1 if the record was updated successfully, 0 otherwise.
+ */
+int update_file(char id[], int cart_sum, char* filename, int is_binary) {
     int elements_num = 0, return_value = 0;
     const long RECORD_SIZE = (long) sizeof(CartRecord);
     CartRecord* record = (CartRecord*) calloc(1, RECORD_SIZE);
@@ -48,56 +82,79 @@ int update_file(char id[], int cart_sum, char* filename) {
         `rb+`: Returns `NULL` pointer
         `wb+`: Creates file
     */
-    FILE *file = fopen(filename, "rb+");
+    FILE *file = fopen(filename, is_binary ? "rb+" : "r+");
     if (!file)
         return 0;
 
-    do {
-        if (!strcmp(record->id, id)) {
-            CartRecord updated_row = new_cart(id, record->name, record->cart + cart_sum);
+    if (is_binary) {
+        do {
+            if (!strcmp(record->id, id)) {
+                CartRecord updated_row = new_cart(id, record->name, record->cart + cart_sum);
+                
+                // Seek back to the beginning of the current record and overwrite it
+                fseek(file, -RECORD_SIZE, SEEK_CUR);
+                fwrite(&updated_row, RECORD_SIZE, 1, file);
+                return_value = 1;
+                break;
+            }
+
+            elements_num = fread(record, RECORD_SIZE, 1, file);
+        } while(elements_num);
+    } else {
+        /* 
+            In text file the system can not seek the pointer in the file such as binary files.
+            So we will have to duplicate the contents of the file into a temporary file and write our updated row instead.
             
-            // Seeking back from the current place in the file to replace the current record
-            fseek(file, -RECORD_SIZE, SEEK_CUR);
-            fwrite(&updated_row, RECORD_SIZE, 1, file);
-            return_value = 1;
-            break;
+            After that we will copy line by line the temp file into the original file.
+        */
+        FILE *temp = tmpfile();
+        char line[TEXT_LINE_LENGTH];
+        
+        // Read each line from the original file
+        while (fgets(line, sizeof(line), file)) {
+            // Parse the line into a `CartRecord` struct by sacnning the line using this format
+            sscanf(line, "%9s %30s %lf", record->id, record->name, &record->cart);
+            
+            if (!strcmp(record->id, id)) {
+                record->cart += cart_sum;
+                return_value = 1;
+            }
+
+            // Write the same or the updated row into the temp file
+            fprintf(temp, "%9s %30s %8.2lf\n", record->id, record->name, record->cart);
         }
 
-        elements_num = fread(record, RECORD_SIZE, 1, file);
-    } while(elements_num);
+        // Rewind the temporary file to the beginning
+        rewind(temp);
+        file = freopen(filename, "w", file); // Reopen the original file in write mode to overwrite it
+        
+        // Write the contents of the temporary file to the original file
+        while (fgets(line, sizeof(line), temp))
+            fputs(line, file);
+        
+        fclose(temp);
+    }
 
+    // Close the original file
     fclose(file);
     return return_value;
 }
 
-char* read_line(FILE* file, int line_max_chars) {
-    char current_char;
-    int i = 0;
-    char* buffer = (char*) calloc(line_max_chars, sizeof(char));
-    
-    do {
-        current_char = getc(file);
-        buffer[i++] = current_char;
-    } while(current_char != EOF && current_char != '\n');
-
-    return buffer;
-}
-
-void read_txt_file(char* filename) {
-    const int LINE_CHAR_LENGTH = 10+31+10;
-    FILE* file = fopen(filename, "r+");
-    if (!file)
+/**
+ * Printing text file line by line
+ * 
+ * @param filename Target file to print
+ */
+void print_txt_file(char* filename) {
+    char current_line[TEXT_LINE_LENGTH];
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("File not exists or can not open it");
         return;
-
-    char* current_line = NULL;
-    do {
-        if (current_line) {
-            printf("%s", current_line);
-            free(current_line);
-        }
-        
-        current_line = read_line(file, LINE_CHAR_LENGTH);
-    } while (!feof(file));
+    }
+    
+    while (fgets(current_line, TEXT_LINE_LENGTH, file))
+        printf("%s", current_line);
 }
 
 int main() {
@@ -114,10 +171,13 @@ int main() {
         create_file(new_cart("234567891", "BBB", 234), filename_txt, 0);
     }
 
-    printf("Is file updated ? %d\n", update_file("134679258", 320, filename_bin));
-    printf("And now? %d", update_file("234567891", 456, filename_bin));
+    printf("Is file updated ? %d\n", update_file("134679258", 320, filename_bin, 1));
+    printf("And now? %d", update_file("234567891", 456, filename_bin, 1));
+    
+    printf("Is file updated ? %d\n", update_file("134679258", 320, filename_txt, 0));
+    printf("And now? %d", update_file("234567891", 456, filename_txt, 0));
 
     printf("File contents:\n");
-    read_txt_file(filename_txt);
+    print_txt_file(filename_txt);
     return 0;
 }
